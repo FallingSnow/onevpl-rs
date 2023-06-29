a3c200d0743050ed967b4df890efbdbab4cb0bfe
Ayrton Sparling <ayrton@sparling.us>
2023-06-29 11:01:15 -0700

    Some progress on frame allocator

--- examples/frame_allocator.rs
+++ examples/frame_allocator.rs
@@ -1,13 +1,18 @@
 ///! This example encodes a yuv file (tests/frozen180.yuv) and produces a HEVC YUV 4:2:0 8 bit file at /tmp/output.hevc
-use std::{path::PathBuf, env};
+use std::{env, path::PathBuf, io};
 
 use intel_onevpl_sys::MfxStatus;
 use onevpl::{
     bitstream::Bitstream,
     constants::{self, IoPattern},
     encode::EncodeCtrl,
-    Loader, MfxVideoParams, frameallocator::FrameAllocator,
+    frameallocator::FrameAllocator,
+    Loader, MfxVideoParams,
 };
+
+use onevpl::{self, vpp::VppVideoParams};
+
+const DEFAULT_BUFFER_SIZE: usize = 1024 * 1024 * 2; // 2MB
 
 #[tokio::main]
 pub async fn main() {
@@ -15,38 +20,23 @@ pub async fn main() {
     tracing_subscriber::fmt::init();
 
     // Open file to read from
-    let mut file = std::fs::File::open("tests/frozen180.yuv").unwrap();
+    let mut file = std::fs::File::open("tests/frozen.hevc").unwrap();
     let mut output_path = PathBuf::from(env::temp_dir());
-    output_path.push("output.hevc");
+    output_path.push("output.yuv");
     let mut output = std::fs::File::create(output_path.as_path()).unwrap();
 
-    let width = 320;
-    let height = 180;
-    let target_kbps = 1000;
     let codec = constants::Codec::HEVC;
 
     let mut loader = Loader::new().unwrap();
 
     // Set software decoding
-    loader.use_hardware(false);
+    loader.use_hardware(true);
 
     // Set decode HEVC
-    loader
-        .set_filter_property(
-            "mfxImplDescription.mfxEncoderDescription.encoder.CodecID",
-            codec,
-            None,
-        )
-        .unwrap();
+    loader.require_decoder(codec);
 
     // Set required API version to 2.2
-    loader
-        .set_filter_property(
-            "mfxImplDescription.ApiVersion.Version",
-            constants::ApiVersion::new(2, 2),
-            None,
-        )
-        .unwrap();
+    loader.use_api_version(2, 2);
 
     let mut session = loader.new_session(0).unwrap();
 
@@ -55,91 +45,86 @@ pub async fn main() {
         dbg!(request.num_frame_min());
         MfxStatus::Unsupported
     }));
+    frame_allocator.set_lock_callback(Box::new(|id, data| {
+        dbg!("Lock callback called");
+        MfxStatus::Unsupported
+    }));
+    frame_allocator.set_unlock_callback(Box::new(|id, data| {
+        dbg!("UnLock callback called");
+        MfxStatus::Unsupported
+    }));
     session.set_allocator(frame_allocator).unwrap();
 
     let mut params = MfxVideoParams::default();
 
-    // Encoding config
-    params.set_codec(codec);
-    params.set_target_usage(constants::TargetUsage::Level4);
-    params.set_rate_control_method(constants::RateControlMethod::VBR);
-    params.set_target_kbps(target_kbps);
-    // 24000/1001 = 23.976 fps
-    params.set_framerate(24000, 1001);
-
-    // Input frame config
-    params.set_fourcc(constants::FourCC::IyuvOrI420);
-    params.set_chroma_format(constants::ChromaFormat::YUV420);
-    params.set_io_pattern(IoPattern::IN_SYSTEM_MEMORY);
-
-    // We must know before hand the size of the frames we are giving to the encoder
-    params.set_height(height);
-    params.set_width(width);
-    params.set_crop(width, height);
-
-    // dbg!(params);
-
-    let mut encoder = session.encoder(params).unwrap();
-
-    // Get the configured params from the encoder
-    let params = encoder.params().unwrap();
-
-    // Create a backing buffer that will contain the bitstream of the encoded output
-    let suggested_buffer_size = params.suggested_buffer_size();
-    let mut buffer: Vec<u8> = vec![0; suggested_buffer_size];
+    let mut buffer: Vec<u8> = vec![0; DEFAULT_BUFFER_SIZE];
     let mut bitstream = Bitstream::with_codec(&mut buffer, codec);
+    let free_buffer_len = (bitstream.len() - bitstream.size() as usize) as u64;
+    let bytes_read = io::copy(
+        &mut io::Read::take(&mut file, free_buffer_len),
+        &mut bitstream,
+    )
+    .unwrap();
+    assert_ne!(bytes_read, 0);
+
+    let mfx_params = session
+        .decode_header(&mut bitstream, constants::IoPattern::OUT_VIDEO_MEMORY)
+        .expect("Failed to discover video parameters");
+
+    let mut vpp_params = VppVideoParams::from(&mfx_params);
+    vpp_params.set_io_pattern(
+        constants::IoPattern::VIDEO_MEMORY,
+    );
+    vpp_params.set_out_fourcc(constants::FourCC::YV12);
+
+    let decoder = session.decoder(mfx_params).expect("Unable to create decoder");
+    let vpp = session.video_processor(&mut vpp_params).unwrap();
 
     loop {
+        let frame = match decoder.decode(Some(&mut bitstream), None).await {
+            Ok(frame) => Some(frame),
+            Err(e) if e == MfxStatus::MoreData => {
+                let free_buffer_len = (bitstream.len() - bitstream.size() as usize) as u64;
+                let bytes_read = io::copy(
+                    &mut io::Read::take(&mut file, free_buffer_len),
+                    &mut bitstream,
+                )
+                .unwrap();
 
-        // Gives you additional per frame encoder controls that we won't use in this example
-        let mut ctrl = EncodeCtrl::new();
+                if bytes_read == 0 {
+                    break;
+                }
 
-        // In this example we let the encoder handle the allocation of surfaces
-        let mut frame_surface = encoder.get_surface().unwrap();
-
-        // Read a frame's worth of data from file into the allocated FrameSurface
-        // If we need more data to read one frame, we can assume we are done
-        if let Err(e) = frame_surface.read_raw_frame(&mut file, constants::FourCC::IyuvOrI420).await {
-            match e {
-                MfxStatus::MoreData => break,
-                _ => panic!("{:?}", e),
-            };
-        };
-
-        // Attempt to encode a frame. The encode method returns the number of bytes written to the bitstream. If more data
-        let bytes_written = match encoder
-            .encode(&mut ctrl, Some(frame_surface), &mut bitstream, None)
-            .await
-        {
-            Ok(bytes) => bytes,
-            Err(e) if e == MfxStatus::MoreData => 0,
+                None
+            }
+            Err(e) if e == MfxStatus::VideoParamChanged => {
+                let _params = decoder.params().unwrap();
+                println!("Video decoding parameters changed");
+                None
+            }
             Err(e) => panic!("{:?}", e),
         };
 
-        // If data was written to the bitstream we try to copy the bitstream data to our output file
-        if bytes_written > 0 {
-            let bitstream_size = bitstream.size();
-            let bytes_copied = std::io::copy(&mut bitstream, &mut output).unwrap();
-            assert_eq!(bitstream_size as u64, bytes_copied);
+        if let Some(mut frame) = frame {
+            let mut yuv_frame = vpp.process(Some(&mut frame), None).await.unwrap();
+            let bytes = io::copy(&mut yuv_frame, &mut output).unwrap();
+            assert_ne!(bytes, 0);
         }
     }
 
-    println!("Flushing encoder");
-
+    // Now the flush the decoder pass None to decode
+    // "The application must set bs to NULL to signal end of stream. The application may need to call this API function several times to drain any internally cached frames until the function returns MFX_ERR_MORE_DATA."
     loop {
-        let mut ctrl = EncodeCtrl::new();
-        let bytes_written = match encoder.encode(&mut ctrl, None, &mut bitstream, None).await {
-            Ok(bytes) => bytes,
-            Err(e) if e == MfxStatus::MoreData => break,
+        let mut frame = match decoder.decode(None, None).await {
+            Ok(frame) => frame,
+            Err(e) if e == MfxStatus::MoreData => {
+                break;
+            }
             Err(e) => panic!("{:?}", e),
         };
-
-        if bytes_written > 0 {
-            let bitstream_size = bitstream.size();
-            let bytes_copied = std::io::copy(&mut bitstream, &mut output).unwrap();
-            assert_eq!(bitstream_size as u64, bytes_copied);
-        }
+        let mut yuv_frame = vpp.process(Some(&mut frame), None).await.unwrap();
+        io::copy(&mut yuv_frame, &mut output).unwrap();
     }
 
-    println!("Encoded file was written to: {}", output_path.display());
+    println!("Decoded file was written to: {}", output_path.display());
 }
